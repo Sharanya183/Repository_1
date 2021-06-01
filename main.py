003b71c599a8db2099f2e33df66cb3c0fb3f91df
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2021-06-01 18:57:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+Name = "Sharanya"
+Age = "14"
+Hobbies = "reading Harry Potter, watching Harry Potter, listening to music, blogging, riding, etc., etc.  "
+Text = Name + ' is ' + Age + ". " + Name + " loves " + Hobbies
+print(Text)
+Text2 = Name + ' is now trying to learn python and programming. '
