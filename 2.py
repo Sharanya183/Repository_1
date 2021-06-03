3fab244d26d577a204790a7c70df06a73e510055
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2021-06-03 19:52:52 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+Name = "Crabbe"
+if Name == "Harry":
+    print("Well, hello Mr Potter.")
+elif Name == "Ron":
+    print("Well, how're you doing Mr Weasley?")
+elif Name == "Hermione":
+    print("Hello Miss Granger.")
+else:
+    print("You don't belong here. Get out!!!")
+Age = 7
+if Age>13:
+    print("Hey There!!!")
+else:
+    print("You're not old enough kiddo.")
+    
