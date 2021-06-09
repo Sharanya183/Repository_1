d4f2317b90756eb69ec6d479bbda1596428e9ff8
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2021-06-09 19:47:47 +0530

    Add files via upload

---
+++ nested_while.py
@@ -0,0 +1,10 @@
+x = 1
+while x < 11 :
+    print(x , end = '')
+    x = x + 1
+    y = 1
+    while y < 2:
+        print(" Sheep ")
+        y=y+1
+
+print("Good Night...Zzz")
---
+++ while.py
@@ -0,0 +1,8 @@
+num = 1
+while num < 10:
+    print("Loop", num)
+    num = num + 1
+    while num <= 10:
+        print(num)
+        num += 1
+        
