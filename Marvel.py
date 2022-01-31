6de59d7f7a0a0125e5159e21127b5e2097762be8
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-01-31 13:05:08 +0530

    Add files via upload

---
+++ Marvel.py
@@ -0,0 +1,21 @@
+#Avengers are Evergreen#
+
+Name = input(str("Enter your name: "))
+if Name == "Thanos":
+    print("Get out, loser!")
+elif Name == "Peter Parker":
+    print("What up, Spider-man?")
+elif Name == "Tony Stark":
+    print("Hello Iron Man")
+elif Name == "Steve Rogers":
+    print("Hey Captain!")
+elif Name == "Natasha Romanoff":
+    print("Welcome Black Widow")
+elif Name == "Bruce Banner":
+    print("The Hulk is here!")
+elif Name == "Stephen Strange":
+    print("There's the wizard!")
+elif Name == "Wanda Maximoff":
+    print("Scarlet's here! Just kidding. Welcome Scarlet Witch")
+else:
+    print("Not the original dude.")
