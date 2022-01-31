7783af58dbe64e8f43ac47faf3ac080f3455b82e
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-01-31 12:29:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-Name = "Sharanya"
-Age = "14"
-Hobbies = "reading Harry Potter, watching Harry Potter, listening to music, blogging, riding, etc., etc.  "
-Text = Name + ' is ' + Age + ". " + Name + " loves " + Hobbies
-print(Text)
-Text2 = Name + ' is now trying to learn python and programming. '
+print("Hello World")
+num = []
+num.append(1)
+num.append(23)
+num.append(0)
+print(num)
+print(sorted(num))
+
+
