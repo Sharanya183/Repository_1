bbf861363a2dd87d695ccf3a4e37c7620adb1075
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-06 22:05:50 +0530

    Add files via upload

---
+++ 6feb2022.py
@@ -0,0 +1,12 @@
+#We check in a patient named John Smith. He's 20 years old and a new patient#
+Name = "John Smith"
+Age = 20
+existing_patient = False
+new_patient = True
+
+#Ask 2 questions: Person's name and favorite color. Then print a message like,"Mosh likes Blue."#
+
+Name = input("What is your name? ")
+Favorite_Color = input("What's your favorite color? ")
+print(Name + " likes " + Favorite_Color + ".")
+
