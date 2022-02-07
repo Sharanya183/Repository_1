74a859b48b2092d1024ab585b80a0388b3039087
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-07 19:19:22 +0530

    Add files via upload

---
+++ 7feb2022.py
@@ -0,0 +1,6 @@
+#Ask a user their weight(in pounds), convert it to kilograms and print it on the terminal#
+Weight_lbs = float(input("Enter your weight(lbs): "))
+Weight_kg = Weight_lbs * 0.453
+print("Your weight in kilograms is " + str(Weight_kg))
+
+
