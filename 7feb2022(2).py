2878e0bc5b5253db9d642665aa4f39b0f22d3814
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-07 19:47:31 +0530

    Add files via upload

---
+++ 7feb2022(2).py
@@ -0,0 +1,8 @@
+#String Methods#
+jb = "Justin Bieber's 'Justice'"
+print(jb.title())
+print(jb.find("Bieber"))
+print(jb.lower())
+print(jb.upper())
+print(jb.replace('Justice', 'Changes'))
+'Justice' in jb
