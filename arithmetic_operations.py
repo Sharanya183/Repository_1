79cbd19d678c7e95f1dea5e16169ae155af8b035
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-08 10:59:03 +0530

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,26 @@
+#Arithmetic Operations#
+Add = 109+10
+print(Add)
+Subtract = 109-10
+print(Subtract)
+Multiply = 109*10
+print(Multiply)
+Div = 109/10
+print(Div)
+Divide_int = 109//10
+print(Divide_int)
+Remainder = 109%10
+print(Remainder)
+Exponent = 109**10
+print(Exponent)
+
+x = 1000
+x *= 10
+print(x)
+x += 12
+print(x)
+x -= 109
+print(x)
+x /= 1092
+print(x)
+
