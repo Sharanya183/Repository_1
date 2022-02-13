87f388d4481607a684fbac37e5575e2e61dd57be
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-13 19:06:35 +0530

    Add files via upload

---
+++ Weathertexts.py
@@ -0,0 +1,12 @@
+#Weather texts#
+
+Weather = input("Weather: ")
+if Weather == "Hot":
+    print('''It's a hot day.
+Stay hydrated''')
+elif Weather == "Cold":
+    print('''It's a cold day.
+Wear warm clothes and stay cozy.''')
+else:
+    print("It's a lovely day!")
+
