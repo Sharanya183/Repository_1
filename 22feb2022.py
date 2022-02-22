35ad9d3b81d9cef681751a1bc656a50bbe36760f
Sharanya <84262011+Sharanya183@users.noreply.github.com>
2022-02-22 21:49:14 +0530

    Add files via upload

---
+++ 22feb2022.py
@@ -0,0 +1,6 @@
+has_high_income = True
+has_good_credit = False
+if has_high_income == True or has_good_credit == True:
+    print("Eligible for loan.")
+elif has_good_credit == False and has_high_income == False:
+    print("Not eligible for loan.")
